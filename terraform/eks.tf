93084db38306a420cbe8f37282e69f79725ff435
mirzanikhath <mirzanikhathsultana@gmail.com>
2025-04-17 17:45:49 +0530

    updated some changes

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -51,12 +51,23 @@ module "eks" {
         Environment = "dev"
         ExtraTag = "e-commerce-app"
       }
+      # Attach security group rule for NodePort range (30000-32000)
+      security_group_ids = [module.eks.node_security_group_id]
     }
   }
  
   tags = local.tags
 
 
+}
+resource "aws_security_group_rule" "allow_nodeport_range" {
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 32000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Open to all IPs, restrict as needed
+  security_group_id = module.eks.node_security_group_id
+  description       = "Allow NodePort range 30000-32000 for NGINX Ingress"
 }
 
 data "aws_instances" "eks_nodes" {
@@ -68,6 +79,33 @@ data "aws_instances" "eks_nodes" {
     name   = "instance-state-name"
     values = ["running"]
   }
+
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_name
+
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_name
+
+  depends_on = [module.eks]
+}
+
+resource "helm_release" "jenkins" {
+  name       = "jenkins"
+  namespace  = "jenkins"
+  repository = "https://charts.jenkins.io"
+  chart      = "jenkins"
+  version    = "5.0.9"
+  create_namespace = true
+
+  values = [
+    file("${path.module}/jenkins.yml")
+  ]
 
   depends_on = [module.eks]
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,3 +18,11 @@ provider "aws" {
   region = local.region
 
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.eks.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
+}

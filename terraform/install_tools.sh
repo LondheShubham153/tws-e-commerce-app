b5642fb5a0ba4663ac4c40b1d96ed7981deb2e51
Ahamad <ahamadb224@gmail.com>
2025-04-18 21:17:13 -0400

    Added Sonarqube Installation

--- terraform/install_tools.sh
+++ terraform/install_tools.sh
@@ -42,3 +42,37 @@ sudo snap install helm --classic
 
 # Kubectl installation
 sudo snap install kubectl --classic
+
+#Postgresql Installation for sonarqube
+sudo apt install -y postgresql-common postgresql -y
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
+
+#Creating the user in postgres for sonarqube
+#NOTE: Need to create the password and add schema permission manually for this user after VM got provisioned.
+#since we are using the terraform script in the same repo, if we are using different repo and pipeline configured, we can pull from env or Vault
+#STEPS to create password && allow schema permission
+#1. sudo -u postgres psql  ## It will open the interactive terminal for postgresql
+#2. ALTER ROLE sonaruser WITH PASSWORD 'your_new_password';
+#3. \c sonarqube
+#4. GRANT ALL PRIVILEGES ON SCHEMA public TO sonaruser;
+#5. \q
+sudo -u postgres psql -c "CREATE USER sonaruser WITH LOGIN;"
+sudo -u postgres psql -c "CREATE DATABASE sonarqube OWNER sonaruser;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sonarqube TO sonaruser;"
+
+#SONARQUBE INSTALLATION
+#Since Java already installed for Jenkins, no need to install Java
+#Few configuration changes need to change after VM got created. Look into Readme file
+sudo apt update
+sudo apt install unzip
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-25.2.0.102705.zip
+unzip sonarqube-25.2.0.102705.zip
+sudo mv sonarqube-25.2.0.102705/ /opt/sonarqube
+sudo adduser --system --no-create-home --group --disabled-login sonarqube
+sudo chown -R sonarqube:sonarqube /opt/sonarqube
+
+#SONARSCANNER INSTALLATION
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-7.0.1.4817-linux-x64.zip
+unzip sonar-scanner-cli-7.0.1.4817-linux-x64.zip
+sudo mv sonar-scanner-7.0.1.4817-linux-x64/  /opt/sonarscanner
